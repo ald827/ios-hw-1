4727e78adf88fcbd3419007faae61bdbd436ef76
ald278 <dealdaihane1@gmail.com>
2020-09-11 20:42:24 +0300

    dene

--- iOS-Hw1-Part1.playground/Contents.swift
+++ iOS-Hw1-Part1.playground/Contents.swift
@@ -5,9 +5,9 @@
  
  */
 
-let name = "Ahmad"
+var name = "Ahmad"
 
-name = "Salem"
+ name = "Salem"
 
 
 var myWeight = 60
@@ -15,11 +15,11 @@ var FriendWeight = 80.5
 var height = 1.5
 
 
-var weightSum = myWeight + FriendWeight
+var weightSum = Double(myWeight) + FriendWeight
 
 
 
-var bmi = myWeight * (height * height)
+var bmi = Double(myWeight) * (height * height)
 
 /*
 - - - -
@@ -38,20 +38,27 @@ var bmi = myWeight * (height * height)
  */
 
 //Person 1
-var firstName1 = ""
-var lastName1 = ""
-var email1 = ""
-var phoneNumber1 = ""
-var age1 = 0
-var country1 = ""
-var area1 = ""
-var password = ""
-var isKuwaiti = false
+var firstName1 = "deemah"
+var lastName1 = "aldaihane"
+var email1 = "dealdaihane1@gmail.com"
+var phoneNumber1 = "12345678"
+var age1 = 17
+var country1 = "kw"
+var area1 = "hadiyya"
+var password = "12345678"
+var isKuwaiti = true
 
 //Person 2
 
-
-
+var firstName2 = "aldana"
+var lastName2 = "alrashidi"
+var email2 = "aldana@gmail.com"
+var phoneNumber2 = "12345678"
+var age2 = 16
+var country2 = "kw"
+var area2 = "faihaa"
+var password2 = "12345678"
+var isKuwaiti2 = true
 
 
 
@@ -60,9 +67,15 @@ var isKuwaiti = false
 
 
 //Person 3
-
-
-
+var firstName3 = "sara"
+var lastName3 = "alafasi"
+var email3 = "sara@gmail.com"
+var phoneNumber3 = "12345678"
+var age3 = 15
+var country3 = "kw"
+var area3 = "yarmook"
+var password3 = "12345678"
+var isKuwaiti3 = true
 
 
 
@@ -80,5 +93,66 @@ var isKuwaiti = false
  * قارن اذا عمر الاشخاص يساوي  ١٧
  * قارن اذا عمر الاشخاص لا يساوي  ١٧
 
- 
+
  */
+
+age1 == age2
+age1 == age3
+age2 == age3
+
+area1 == area2
+area1 == area3
+area2 == area3
+
+ age1 > 18
+ age2 > 18
+ age3 > 18
+
+age1 < 18
+age2 < 18
+age3 < 18
+
+age1 == 17
+age2 == 17
+age3 == 17
+
+age1 != 17
+age2 != 17
+age3 != 17
+
+age1 == age2 || age1 == age3 || age2 == age3
+
+area1 == area2 || area1 == area3 || area2 == area3
+
+age1 > 18 || age2 > 18 || age3 > 18
+
+age1 < 18 || age2 < 18 || age3 < 18
+
+age1 == 17 || age2 == 17 || age3 == 17
+
+age1 != 17 || age2 != 17 || age3 != 17
+
+
+
+
+print(" *  * ")
+print("*    *")
+print(" *  *                 ******            * *")
+print("                   *         *        *     * ")
+print("  *               *           *      *       *")
+print("  *               *           *     *         *")
+print("  *               *           *     *")
+print("  *               *           *      *")
+print("  *               *           *        *")
+print("  *               *           *          *  ")
+print("  *               *           *            *")
+print("  *               *           *               * ")
+print("  *               *           *                  *")
+print("  *               *           *       *            *    ")
+print("  *               *           *       *            *  ")
+print("  *                *         *         *          * ")
+print("  *                 *       *           *       * ")
+print("  *                  ******               *  *  ")
+
+
+
